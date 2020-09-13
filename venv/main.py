fb591259b82e5ce761dec539b7c38c285d5f8c19
ozgeonec <ozgenuronec@gmail.com>
2020-09-13 17:15:34 +0300

    gray, blur, canny, dialation, eroded operations on an image

--- venv/main.py
+++ venv/main.py
@@ -1,17 +1,28 @@
 import cv2
-import numpy
+import numpy as np
 
+kernel = np.ones((5,5),np.uint8)
 
-#img=cv2.imread("Resources/stronggirl.jpg")
-#cv2.imshow("Output",img)
-#cv2.waitKey(0)
+img=cv2.imread("Resources/stronggirl.jpg")
+imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+imgBlur = cv2.GaussianBlur(imgGray,(7,7),0)
+imgCanny = cv2.Canny(img,150,200)
+imgDialation = cv2.dilate(imgCanny,kernel,iterations=1)
+imgEroded = cv2.erode(imgDialation,kernel,iterations=1)
 
-cap = cv2.VideoCapture(0)
-cap.set(3,640)
-cap.set(4,480)
-cap.set(10,300)
-while True:
-    success, img = cap.read()
-    cv2.imshow("Video",img)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+cv2.imshow("Gray Image",imgGray)
+cv2.imshow("Blur Image",imgBlur)
+cv2.imshow("Canny Image",imgCanny)
+cv2.imshow("Dialation Image", imgDialation)
+cv2.imshow("Eroded Image", imgEroded)
+cv2.waitKey(0)
+
+#cap = cv2.VideoCapture(0)
+#cap.set(3,640)
+#cap.set(4,480)
+#cap.set(10,300)
+#while True:
+    #success, img = cap.read()
+    #cv2.imshow("Video",img)
+    #if cv2.waitKey(1) & 0xFF == ord('q'):
+        #break
