d47be85cc0a019bbf1a523ed742e63d1c87b1963
ozgeonec <ozgenuronec@gmail.com>
2020-09-13 14:11:22 +0300

    played a video

--- venv/main.py
+++ venv/main.py
@@ -1,15 +1,15 @@
 import cv2
 import numpy
 
-print("import")
-img=cv2.imread("Resources/stronggirl.jpg")
-cv2.imshow("Output",img)
-cv2.waitKey(0)
+
+#img=cv2.imread("Resources/stronggirl.jpg")
+#cv2.imshow("Output",img)
+#cv2.waitKey(0)
 
 cap = cv2.VideoCapture("Resources/chewievideo.mp4")
 
 while True:
-    success, imag = cap.read()
-    cv2.imshow("Video",imag)
+    success, img = cap.read()
+    cv2.imshow("Video",img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
