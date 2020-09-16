634f800e63f054f0dd549ab0169bb339620275e4
ozgeonec <ozgenuronec@gmail.com>
2020-09-16 14:11:50 +0300

    Shape Detection Process 3-Applying operations to recognize shapes continued

--- venv/main.py
+++ venv/main.py
@@ -42,7 +42,7 @@ def getContours(img):
         #print(area)
         #Check treshold
     #if area>2:
-        cv2.drawContours(imgContour,cnt,-1,(255,0,0),3)
+        cv2.drawContours(imgCanny,cnt,-1,(255,0,0),3)
         prmtr = cv2.arcLength(cnt,True)
         cornerPoints = cv2.approxPolyDP(cnt,0.02*prmtr,True)
         print(len(cornerPoints))
@@ -50,14 +50,17 @@ def getContours(img):
         x, y, w, h = cv2.boundingRect(cornerPoints)
 
         if corners == 3: objType = "Triangle"
-        elif corners == 4: objType= "Rectangle"
+        elif corners == 4:
+             aspRatio = w/float(h)
+             if aspRatio >0.98 and aspRatio <1.03: objectType= "Square"
+             else: objType= "Rectangle"
         elif corners > 4: objType= "Circle"
         else: objType= "None"
 
 
         cv2.rectangle(imgContour,(x,y),(x+w,y+h),(0,255,0),1)
         cv2.putText(imgContour,objType,
-                    (x+(w//2)-10,y+(h//2)-10),cv2.FONT_HERSHEY_COMPLEX,0.5,(0,0,0),1)
+                    (x+(w//2)-10,y+(h//2)-10),cv2.FONT_HERSHEY_COMPLEX,0.7,(0,0,0),1)
 
 
 ##############Shape Detection###################
