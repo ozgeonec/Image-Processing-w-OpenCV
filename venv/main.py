d1e3242095c5bf90506e508686439310a9767d75
ozgeonec <ozgenuronec@gmail.com>
2020-09-16 14:32:47 +0300

    Face Detection

--- venv/main.py
+++ venv/main.py
@@ -3,80 +3,88 @@ import numpy as np
 
 
 ################Copied Stacking Function###############
-def stackImages(scale,imgArray):
-    rows = len(imgArray)
-    cols = len(imgArray[0])
-    rowsAvailable = isinstance(imgArray[0], list)
-    width = imgArray[0][0].shape[1]
-    height = imgArray[0][0].shape[0]
-    if rowsAvailable:
-        for x in range ( 0, rows):
-            for y in range(0, cols):
-                if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
-                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
-                else:
-                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
-                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
-        imageBlank = np.zeros((height, width, 3), np.uint8)
-        hor = [imageBlank]*rows
-        hor_con = [imageBlank]*rows
-        for x in range(0, rows):
-            hor[x] = np.hstack(imgArray[x])
-        ver = np.vstack(hor)
-    else:
-        for x in range(0, rows):
-            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
-                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
-            else:
-                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None,scale, scale)
-            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
-        hor= np.hstack(imgArray)
-        ver = hor
-    return ver
+# def stackImages(scale,imgArray):
+#     rows = len(imgArray)
+#     cols = len(imgArray[0])
+#     rowsAvailable = isinstance(imgArray[0], list)
+#     width = imgArray[0][0].shape[1]
+#     height = imgArray[0][0].shape[0]
+#     if rowsAvailable:
+#         for x in range ( 0, rows):
+#             for y in range(0, cols):
+#                 if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
+#                     imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
+#                 else:
+#                     imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
+#                 if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
+#         imageBlank = np.zeros((height, width, 3), np.uint8)
+#         hor = [imageBlank]*rows
+#         hor_con = [imageBlank]*rows
+#         for x in range(0, rows):
+#             hor[x] = np.hstack(imgArray[x])
+#         ver = np.vstack(hor)
+#     else:
+#         for x in range(0, rows):
+#             if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
+#                 imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
+#             else:
+#                 imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None,scale, scale)
+#             if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+#         hor= np.hstack(imgArray)
+#         ver = hor
+#     return ver
 
 #############Contour Ops##############
-def getContours(img):
-    contours,hierarchy = cv2.findContours(img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
-    for cnt in contours:
-        area = cv2.contourArea(cnt)
-        #print(area)
-        #Check treshold
-    #if area>2:
-        cv2.drawContours(imgCanny,cnt,-1,(255,0,0),3)
-        prmtr = cv2.arcLength(cnt,True)
-        cornerPoints = cv2.approxPolyDP(cnt,0.02*prmtr,True)
-        print(len(cornerPoints))
-        corners = len(cornerPoints)
-        x, y, w, h = cv2.boundingRect(cornerPoints)
-
-        if corners == 3: objType = "Triangle"
-        elif corners == 4:
-             aspRatio = w/float(h)
-             if aspRatio >0.98 and aspRatio <1.03: objectType= "Square"
-             else: objType= "Rectangle"
-        elif corners > 4: objType= "Circle"
-        else: objType= "None"
-
-
-        cv2.rectangle(imgContour,(x,y),(x+w,y+h),(0,255,0),1)
-        cv2.putText(imgContour,objType,
-                    (x+(w//2)-10,y+(h//2)-10),cv2.FONT_HERSHEY_COMPLEX,0.7,(0,0,0),1)
+# def getContours(img):
+#     contours,hierarchy = cv2.findContours(img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+#     for cnt in contours:
+#         area = cv2.contourArea(cnt)
+#         #print(area)
+#         #Check treshold
+#     #if area>2:
+#         cv2.drawContours(imgCanny,cnt,-1,(255,0,0),3)
+#         prmtr = cv2.arcLength(cnt,True)
+#         cornerPoints = cv2.approxPolyDP(cnt,0.02*prmtr,True)
+#         print(len(cornerPoints))
+#         corners = len(cornerPoints)
+#         x, y, w, h = cv2.boundingRect(cornerPoints)
+#
+#         if corners == 3: objType = "Triangle"
+#         elif corners == 4:
+#              aspRatio = w/float(h)
+#              if aspRatio >0.98 and aspRatio <1.03: objectType= "Square"
+#              else: objType= "Rectangle"
+#         elif corners > 4: objType= "Circle"
+#         else: objType= "None"
+#
+#
+#         cv2.rectangle(imgContour,(x,y),(x+w,y+h),(0,255,0),1)
+#         cv2.putText(imgContour,objType,
+#                     (x+(w//2)-10,y+(h//2)-10),cv2.FONT_HERSHEY_COMPLEX,0.7,(0,0,0),1)
 
 
 ##############Shape Detection###################
-img=cv2.imread("Resources/shapes.png")
-imgContour = img.copy()
+
+faceCascade= cv2.CascadeClassifier("Resources/haarcascades/haarcascade_frontalface_default.xml")
+img=cv2.imread("Resources/stronggirl.jpg")
+imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+faces = faceCascade.detectMultiScale(imgGray,1.1,4)
+
+for (x,y,w,h) in faces:
+    cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+# imgContour = img.copy()
 
 # def empty(a):
 #      pass
 
 
-
-imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-imgBlur = cv2.blur(imgGray,(5,5),1)
-imgStack = stackImages(0.5,([img,imgGray,imgBlur]))
-imgCanny = cv2.Canny(imgBlur,15,305)
-getContours(imgCanny)
+#########Shape Detection Part###########
+# imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+# imgBlur = cv2.blur(imgGray,(5,5),1)
+# imgStack = stackImages(0.5,([img,imgGray,imgBlur]))
+# imgCanny = cv2.Canny(imgBlur,15,305)
+# getContours(imgCanny)
 
 # while True:
 #  imgHSV = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
@@ -139,11 +147,11 @@ getContours(imgCanny)
 #  imgStack = stackImages(0.6,([img,imgHSV],[mask,imgResult]))
 #  cv2.imshow("Stacked Images", imgStack)
 
-cv2.imshow("Image Stack",imgStack)
-cv2.imshow("Image Canny",imgCanny)
-cv2.imshow("Image Contour",imgContour)
+# cv2.imshow("Image Stack",imgStack)
+# cv2.imshow("Image Canny",imgCanny)
+# cv2.imshow("Image Contour",imgContour)
 
-
+cv2.imshow("Image",img)
 cv2.waitKey(0)
 
 
