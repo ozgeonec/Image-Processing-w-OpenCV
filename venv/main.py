5131cd754ff95c140fa8dbc8aa8e7d6b21ce0650
ozgeonec <ozgenuronec@gmail.com>
2020-09-18 21:42:12 +0300

    Real-time color detection continued

--- venv/main.py
+++ venv/main.py
@@ -9,6 +9,10 @@ cap.set(3, frameWidth)
 cap.set(4, frameHeight)
 
 myColors=[[5,107,0,19,255,255],[133,56,0,159,156,255],[57,76,0,100,255,255]]
+myColorValues = [[51,153,255],          ## BGR
+                 [255,0,255],
+                 [0,255,0],
+                 [255,0,0]]
 # def empty(a):
 #     pass
 
@@ -21,16 +25,33 @@ myColors=[[5,107,0,19,255,255],[133,56,0,159,156,255],[57,76,0,100,255,255]]
 # cv2.createTrackbar("VALUE Min", "HSV", 0, 255, empty)
 # cv2.createTrackbar("VALUE Max", "HSV", 255, 255, empty)
 #0,179,145,255,103,255
-def findColors(img,myColors):
+def findColors(img,myColors,myColorValues):
+    count = 0
     imgHsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     for color in myColors:
       lower = np.array(color[0:3])
       upper = np.array(color[3:6])
       mask = cv2.inRange(imgHsv, lower, upper)
+      x,y=getContours(mask)
+      cv2.circle(imgResult,(x,y),15,myColorValues[count],cv2.FILLED)
       cv2.imshow(str(color[0]),mask)
+      count += 1
 
+def getContours(img):
+    contours,hierarchy = cv2.findContours(img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+    x,y,w,h=0,0,0,0
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        #Check treshold
+    #if area>2:
+        cv2.drawContours(imgResult,cnt,-1,(255,0,0),3)
+        prmtr = cv2.arcLength(cnt,True)
+        cornerPoints = cv2.approxPolyDP(cnt,0.02*prmtr,True)
+        x, y, w, h = cv2.boundingRect(cornerPoints)
+    return x*w//2,y
 while True:
     success, img = cap.read()
+    imgResult = img.copy()
     #imgHsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
     # h_min = cv2.getTrackbarPos("HUE Min", "HSV")
@@ -47,8 +68,8 @@ while True:
     # mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
     # hStack = np.hstack([img, mask, result])
     # cv2.imshow('Horizontal Stacking', hStack)
-    findColors(img,myColors)
-    cv2.imshow("Result",img)
+    findColors(img,myColors,myColorValues)
+    cv2.imshow("Result",imgResult)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
